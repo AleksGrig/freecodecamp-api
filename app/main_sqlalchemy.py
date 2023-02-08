495f5053d78bec4db9ded7258132805b20d374f9
AleksGrig <ullitap@gmail.com>
2023-02-08 09:48:24 +0300

    votes

--- app/main_sqlalchemy.py
+++ app/main_sqlalchemy.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from . import database, models
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 from .config import settings
 
 models.Base.metadata.create_all(bind=database.engine)
@@ -9,6 +9,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root():
--- app/routers/vote.py
+++ app/routers/vote.py
@@ -18,6 +18,15 @@ def vote(vote: schemas.Vote,
   if vote.dir == 1:
     if found_vote:
       raise HTTPException(status_code=status.HTTP_409_CONFLICT, 
-                          detail=f"user with id {current_user.id} has already voted on post {vote.post_id}")                             
+                          detail=f"user with id {current_user.id} has already voted on post {vote.post_id}")  
+    new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
+    db.add(new_vote)
+    db.commit()
+    return {"message": "successfully added vote"}                           
   else:
-    pass
+    if not found_vote:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                          detail=f"vote doesn't exist")
+    vote_query.delete(synchronize_session=False)
+    db.commit()
+    return {"message": "successfully deleted vote"}
