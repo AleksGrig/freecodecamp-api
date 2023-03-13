b4028409afedc901be909aa62c64cd4a9783344e
AleksGrig <ullitap@gmail.com>
2023-03-13 17:17:04 +0300

    login user test added

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,21 @@
 import requests
+import pytest
 from app import schemas
 from .database import client, session
+
+
+@pytest.fixture
+def test_user(client):
+    userdata = {
+        "email": "hello123@gmail.com",
+        "password": "hello123"
+    }
+    res = client.post("http://192.168.1.65/users/", json=userdata)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = userdata['password']
+    return new_user
+
 
 def test_root(client):
     # res = requests.get("http://192.168.1.65/")
@@ -21,3 +36,12 @@ def test_create_user(client):
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
+
+
+# we send credentials using form-data, not json!!!
+def test_login_user(client, test_user):
+    res = client.post("/login", data={
+        "username": test_user['email'],
+        "password": test_user['password']
+    })
+    assert res.status_code == 200
