84f498d13140fd49908d5efecbfc672fb5b857a3
AleksGrig <ullitap@gmail.com>
2023-03-13 16:18:31 +0300

    moved database stuff in separate file

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,47 +1,6 @@
 import requests
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from app import schemas
-from app.config import settings
-from app.main import app
-from app.database import get_db
-from app.models import Base
-
-
-SQLALCHEMY_DATABASE_URL = 'mysql://root:1234@192.168.1.65:3306/fastapi_test'
-# SQLALCHEMY_DATABASE_URL = f'mysql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    # run our code before out test
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-
-    # run our code after test finishes
-
+from .database import client, session
 
 def test_root(client):
     # res = requests.get("http://192.168.1.65/")
