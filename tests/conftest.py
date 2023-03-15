34249264c3b6b63ccfdd21342f70af98b1142a34
AleksGrig <ullitap@gmail.com>
2023-03-15 13:51:37 +0300

    add 2nd user+post

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,6 +57,19 @@ def test_user(client):
 
 
 @pytest.fixture
+def test_user2(client):
+    userdata = {
+        "email": "hello234@gmail.com",
+        "password": "hello234"
+    }
+    res = client.post("http://192.168.1.65/users/", json=userdata)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = userdata['password']
+    return new_user
+
+
+@pytest.fixture
 def token(test_user):
     return create_access_token({"user_id": test_user['id']})
 
@@ -71,7 +84,7 @@ def authorized_client(client, token):
 
 
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, test_user2, session):
     posts_data = [
         {
             "title": "first_title",
@@ -87,7 +100,12 @@ def test_posts(test_user, session):
             "title": "3rd_title",
             "content": "3rd_content",
             "owner_id": test_user['id']
-        }
+        },
+        {
+            "title": "4th_title",
+            "content": "4th_content",
+            "owner_id": test_user2['id']
+        },
     ]
     posts = [models.Post(**post_data) for post_data in posts_data]
     session.add_all(posts)
